b96c9e68b4202835672b939b19b8db9d65dcb0e0
AaronTown <aaron.town16@gmail.com>
2024-07-23 22:07:35 -0700

    Adding Cursor + Death & Restart Screen

--- Code/Game.gd
+++ Code/Game.gd
@@ -4,6 +4,7 @@ var money : int = 0
 const max_energy : float = 2.0
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,7 +14,15 @@ func _ready():
 func _process(delta):
 	money += 1
 	$HealthIndicator.energy = max_energy * ($Emitter.max_health - $Emitter.health) / $Emitter.max_health
+	if $Emitter.health <= 0:
+		Game_Over()
 	$GUI/Money.text = "Money: " + str(money)
 
 func AddMoney(amount):
 	money += amount
+
+# This function will play out the steps for when a GameOver occurs.
+# The factors to call this function will be called in _process(delta)
+func Game_Over():
+	var game_over = $GameOver
+	game_over.show()
